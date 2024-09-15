c8c98517236478433db63063faf7b7c3c0b4ad39
paytongithub <trp31604@gmail.com>
2024-09-15 17:48:55 -0500

    Revert "transferring to coding on Pi from vsc"
    This reverts commit e08c55ef02a740a7c3fc76534fad1d262b744c26.

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,17 +1,8 @@
-#include "list.h"
 
-    
 
 
 void kernel_main() {
-    struct list_element c = {NULL, 0};
-    struct list_element b = {&c, 0};
-    struct list_element a = {&b, 0};
-    struct list_element *head = &a;
 
-    struct list_element new_node = {NULL, 1};
-    insert(head, &new_node);
-    remove(head);
     while(1){
     }
 }
