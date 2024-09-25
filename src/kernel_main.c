6d9551058f53fc0e5d115383f7ac097a55766144
Layla Payton <119980609+paytongithub@users.noreply.github.com>
2024-09-25 16:59:55 -0500

    Update kernel_main.c for hw4

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,48 +1,51 @@
+#include "rprintf.c"
+#include "rprintf.h"
 #include "list.c"
-#include <stddef.h>
-#include <stdio.h>
+#include "serial.c"
+
+char glbl[128];
+
+unsigned long get_timer_count() {
+    unsigned long *timer_count_register = 0x3f003004;
+    return *timer_count_register;
+}
 
 
+void wait() {
+    int start = get_timer_count();
+    while(get_timer_count() - start < 1000) {
+    }
+}
 
-void main() {    
+void bss() {
+    extern int __bss_start, __bss_end;
+    char *bssstart, *bssend;
+    bssstart = &__bss_start;
+    bssend = (char *)&__bss_end;
+
+    for (int i = 0; i <= bssend - bssstart; i++) {
+        bssstart[i] = 0;
+    }
+}
+
+
+void kernel_main() {
+	
     wait();
+
+    bss();
+
+    esp_printf(putc, "test");
 
     struct list_element c = { NULL, 3};
     struct list_element b = { &c, 2};
     struct list_element a = { &b, 1};
     struct list_element *head = &a;
     struct list_element test = { NULL, 10};
-/* //test cases  
     list_add(head, &test);
-    printf("%d %d %d %d\n",a.data,a.next->data,a.next->next->data,a.next->next->next->data);   
     list_remove(&c);
-    printf("%d %d %d\n",a.data,a.next->data,a.next->next->data);
-*/
-}
-/*
-void kernel_main() {
-    extern int __bss_start, __bss_end;
 
-    char *bssstart = (char *)&__bss_start;
-    char *bssend = (char *)&__bss_end;
-
-    while (bssstart < bssend) {
-	*bssstart = 0;
-	bssstart++;
-    }
     while(1) {
-    }
-}
-*/
-
-unsigned long get_timer_count () {
-    unsigned long * timer_count_register = 0 x3f003004;
-    return * timer_count_register;
-}
-
-void wait() {
-    int start = get_timer_count();
-    while(get_timer_count() - start < 1000) {
 
     }
 }
